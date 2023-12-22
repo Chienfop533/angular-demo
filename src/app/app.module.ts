d514f6c991cd1735da3b20805bab054332b9f053
Chienfop533 <130647390+Chienfop533@users.noreply.github.com>
2023-12-22 10:45:01 +0700

    setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [AppComponent ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
